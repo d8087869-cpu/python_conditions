27a51dea6d2ba42003bda3f1a123f5cc90f5954d
david <d8087869@gmail.com>
2026-06-21 17:29:01 +0300

    added_cave_function

--- adventuer_trip.py
+++ adventuer_trip.py
@@ -1,8 +1,22 @@
 trip = input("where do you want to go ")
 match trip:
-    case forest: 
+    case "forest": 
         choose = input("where do you want to go , hide or walk ")
         if choose == "hide": 
             print("you hide behind a tree ")
-        if choose == "walk": 
+        elif choose == "walk": 
             print("you find a sleeping wolf")    
+        else:
+            print("invalid forest action")
+    case "cave": 
+        choose = input("do you have a torch ")      
+        if choose == "yes":
+            turn = input("left or rigth ")
+            if turn == "left":
+                print("you find gold ")
+            elif turn == "rigth": 
+                print("you find bats")
+            else:
+                print("invalid cave path")  
+        else:
+            print("its to dark to enter")             

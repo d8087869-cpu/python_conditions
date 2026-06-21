b029b3f6ccbd5a60265a176c66f78e14e5348010
david <d8087869@gmail.com>
2026-06-21 20:55:12 +0300

    added_receipt_option

--- atm.py
+++ atm.py
@@ -5,4 +5,12 @@ if user_pin == correct_pin:
     amount = int(input("enter withdrawal amount:"))
     if amount > balance:
         print("not enough money ")
-        
+    else:
+        receipt = input("do you want a recepit , yes /no")
+        match receipt:
+            case "yes":
+                print("withdrawal with receipt")
+            case "no":
+                print("withdrawal without receipt")
+            case _:
+                print("withdrawal approved")        

5c54929a8de355d8f16a7678d3764b78c2c9d71a
david <d8087869@gmail.com>
2026-06-21 21:33:33 +0300

    addednew-functions

--- food_delivery.py
+++ food_delivery.py
@@ -1,4 +1,13 @@
 order_price = float(input("enter order price"))
 if order_price < 50:
     print("order to small for delivery")
-    
+else:
+    member = input("are you in the club member ? yes,no:") 
+    if member == "yes":
+        coupon = input("do you have a coupon? yes,no")
+        if coupon == "yes":
+            print("free delivery and 10 discount")
+        else:
+            print("free delivery")    
+    else:
+        print("delivery costs 15 ")        

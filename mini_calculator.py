9e0adbcef3da314fe63511488aa7f25ee3208488
david <d8087869@gmail.com>
2026-06-21 18:01:46 +0300

    addedallfunctions

---
+++ mini_calculator.py
@@ -0,0 +1,9 @@
+num_1 = float(input("enter first num"))
+num_2 = float(input("enter second num"))
+action = input(" which action you choose , add , subtract , multiply")
+if action == "add":
+    print(num_1+num_2)
+elif action== "subtract":
+    print(num_1 - num_2)
+elif action=="multiply":
+    print(num_1*num_2)    
